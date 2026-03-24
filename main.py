cbb95d2feaf607ebf270c3b15d5df70977b72d8c
P RAJITH <149134399+rajithpadma@users.noreply.github.com>
2026-03-24 21:29:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -483,4 +483,8 @@ def initialize():
 
 if __name__ == '__main__':
     initialize()
-    app.run(host=HOST, port=PORT, debug=DEBUG)
+    
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    
+    app.run(host="0.0.0.0", port=port, debug=DEBUG)
